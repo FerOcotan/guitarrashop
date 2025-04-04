557c97220ed98a97c4a3ab6a725bccb87b7a3942
Melvin Fernando Ocotan Morales <melvin.ocotan@catolica.edu.sv>
2025-04-04 07:22:43 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1>
+        Vite + React + Tailwind CSS + TypeScript + Vitest + React Testing Library
+      </h1>
+    </>
+  )
+}
+
+export default App
